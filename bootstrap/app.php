b40a90ac36d2e49898873c54db20df3d4e38cc45
Jacek Obst <jacek.obst@merito.pl>
2024-05-11 10:27:22 +0200

    feat(api): force json response on every api route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
