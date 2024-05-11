6e684c75b735c996701ac123c44b4667d96f7931
Jacek Obst <jacek.obst@merito.pl>
2024-05-11 13:13:07 +0200

    feat: validate that date_to must be after date_from

--- app/Modules/Reservation/Requests/PostReservationsRequest.php
+++ app/Modules/Reservation/Requests/PostReservationsRequest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Modules\Reservation\Requests;
 
+use Carbon\CarbonPeriod;
+use Spatie\LaravelData\Attributes\Validation\After;
 use Spatie\LaravelData\Attributes\Validation\Date;
 use Spatie\LaravelData\Data;
 
@@ -13,7 +15,7 @@ final class PostReservationsRequest extends Data
         #[Date]
         public string $date_from,
 
-        #[Date]
+        #[Date, After('date_from')]
         public string $date_to,
     ) {
     }
