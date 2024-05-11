a186cd47f24247c5468e8377ecd234ea6a960169
Jacek Obst <jacek.obst@merito.pl>
2024-05-11 12:04:53 +0200

    feat: create ConflictException

---
+++ app/Http/Exceptions/GeneralException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Exceptions;
+
+use App\Shared\Response\JsonResp;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+abstract class GeneralException extends Exception
+{
+    public function render(): JsonResponse
+    {
+        return JsonResp::custom(
+            $this->getCode(),
+            $this->getMessage()
+        );
+    }
+}
