6dbc2e09bb561c0098d93601a93476d47c8cae9a
Jacek Obst <jacek.obst@merito.pl>
2024-05-11 10:38:33 +0200

    chore: install barryvdh/laravel-ide-helper and generate helper files

---
+++ _ide_helper_models.php
@@ -0,0 +1,97 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	#[\AllowDynamicProperties]
+	class IdeHelperUser {}
+}
+
+namespace App\Modules\Reservation{
+/**
+ * 
+ *
+ * @property string $id
+ * @property string $date_from
+ * @property string $date_to
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Modules\Vacant\Vacant> $vacants
+ * @property-read int|null $vacants_count
+ * @method static \Database\Factories\ReservationFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation whereDateFrom($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation whereDateTo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Reservation whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	#[\AllowDynamicProperties]
+	final class IdeHelperReservation {}
+}
+
+namespace App\Modules\Vacant{
+/**
+ * 
+ *
+ * @property string $id
+ * @property string $date
+ * @property int $number_of_beds
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Modules\Reservation\Reservation> $reservations
+ * @property-read int|null $reservations_count
+ * @method static \Database\Factories\VacantFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant whereNumberOfBeds($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vacant whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	#[\AllowDynamicProperties]
+	final class IdeHelperVacant {}
+}
+
--- app/Modules/Vacant/Vacant.php
+++ app/Modules/Vacant/Vacant.php
@@ -11,6 +11,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @mixin IdeHelperVacant
+ */
 final class Vacant extends Model
 {
     use HasUuids;
