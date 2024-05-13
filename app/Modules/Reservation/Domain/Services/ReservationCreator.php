deb07ada02026c30b0e005d00839e471fd791d0e
Jacek Obst <jacek.obst@merito.pl>
2024-05-13 16:56:38 +0200

    refactor: create ReservationDatesDto

--- app/Modules/Reservation/Domain/Services/ReservationCreator.php
+++ app/Modules/Reservation/Domain/Services/ReservationCreator.php
@@ -8,12 +8,12 @@ use App\Http\Exceptions\ConflictException;
 use App\Modules\Auth\Domain\Models\User;
 use App\Modules\Reservation\Domain\Contracts\ReservationCreatorInterface;
 use App\Modules\Reservation\Domain\Contracts\ReservationModelManagerInterface;
+use App\Modules\Reservation\Domain\DataTransferObjects\ReservationDatesDto;
 use App\Modules\Reservation\Domain\Models\Reservation;
 use App\Modules\Slot\Domain\Contracts\DataTransferObjects\SlotDtoCollection;
 use App\Modules\Slot\Domain\Contracts\SlotGetterInterface;
 use App\Modules\Slot\Domain\Contracts\SlotReserverInterface;
-use Carbon\Carbon;
-use Carbon\CarbonPeriod;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
@@ -36,7 +36,7 @@ final readonly class ReservationCreator implements ReservationCreatorInterface
      */
     public function handle(array $data): Reservation
     {
-        $dates = $this->getArrayOfDays($data['date_from'], $data['date_to']);
+        $dates = (new ReservationDatesDto($data['date_from'], $data['date_to']))->getArrayOfDays();
         $slots = $this->getMatchingFreeSlots($dates);
 
         return DB::transaction(function () use ($data, $slots) {
@@ -48,39 +48,16 @@ final readonly class ReservationCreator implements ReservationCreatorInterface
     }
 
     /**
-     * @return non-empty-list<non-empty-string>
-     *
-     * We're removing last date because that day shouldn't be counted as a day of reservation.
-     * Example:
-     * - we have reservation from 2022-01-01 to 2022-01-03
-     * - reservation is theoretically for 3 days, but in reality we want to count only 2022-01-01 and 2022-01-02
-     *   cause 2022-01-03 can be used in another reservation
-     */
-    private function getArrayOfDays(string $dateFrom, string $dateTo): array // TODO przenieś gdzieś indziej
-    {
-        /** @var non-empty-list<Carbon> $period */
-        $period = CarbonPeriod::create($dateFrom, $dateTo)->toArray();
-
-        array_pop($period);
-
-        /** @var non-empty-list<non-empty-string> */
-        return array_map(
-            static fn(Carbon $date): string => $date->toDateString(),
-            $period
-        );
-    }
-
-    /**
      * @param non-empty-list<non-empty-string> $dates
      *
      * @throws ConflictException
      */
     private function getMatchingFreeSlots(array $dates): SlotDtoCollection
     {
-        $freeSlots = $this->slotGetter->getFreeSlotsByDates($dates);
+        /** @var Collection<int, SlotDtoCollection> $freeSlotsGroupedByAsset */
+        $freeSlotsGroupedByAsset = $this->slotGetter->getFreeSlotsByDates($dates)->groupByAssetId();
 
-        /**@var ?SlotDtoCollection $matchingFreeSlots */
-        $matchingFreeSlots = $freeSlots->groupByAssetId()->first(
+        $matchingFreeSlots = $freeSlotsGroupedByAsset->first(
             static fn(SlotDtoCollection $slots): bool => $slots->count() === count($dates)
         );
 
@@ -106,7 +83,6 @@ final readonly class ReservationCreator implements ReservationCreatorInterface
 
         // just for the purpose of making frontend work without authentication
         $reservation->user_id = auth()->id() ?? User::whereEmail('user@gmail.com')->first()->id;
-
         $reservation->asset_id = $slots->first()->assetId;
         $reservation->total_price = $slots->getTotalPrice();
 
