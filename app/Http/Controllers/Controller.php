6049316b5593a9b2ceaa9684a198aa36dfbe30f8
Jacek Obst <jacek.obst@merito.pl>
2024-05-13 20:11:05 +0200

    chore: install darkaonline/l5-swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *    title="Emoti Booking API",
+ *    version="1.0.0",
+ * )
+ */
 abstract class Controller
 {
     //
